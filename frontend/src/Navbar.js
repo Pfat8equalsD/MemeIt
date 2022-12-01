0121710ea40705d3612bd54edef36eb08810c779
Andrei.bat <marian.sentiment@gmail.com>
2022-12-02 00:35:51 +0200

    Mock navbar for desktop Not yet finished as it still has absolute positioning

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -6,7 +6,9 @@ export default function Navbar() {
 	return (
 
 		<div className="navbar">
-
+			<div className="logo"></div>
+			<div className="navbutton">Creare cont</div>
+			<div className="navbutton navlogin">Logare</div>
 		</div>
 
 		
