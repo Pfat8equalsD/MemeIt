eed314f9c0ceca8ed88612b39049d0372e92d14b
Andrei.bat <marian.sentiment@gmail.com>
2022-12-13 14:13:29 +0200

    Adding placeholder login and create account button

--- frontend/src/MyNavbar.js
+++ frontend/src/MyNavbar.js
@@ -5,27 +5,46 @@ import Nav from 'react-bootstrap/Nav'
 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 function MyNavbar() {
   return (
-    <Navbar>
-      <Container>
-        <Navbar.Brand href="#home">
+    <Navbar collapseOnSelect expand="lg">
+        <Navbar.Brand style={{marginLeft:30}} href="#home">
 			<img
-				src="img/MemeIT.png"
+				src={require('./img/MemeIT.png')}
 				className="d-inline-block align-top"
 			/>
 		</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav.Link href="#home">
-			Logare
-          </Nav.Link>
+		<Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end ml-auto">
+			<Nav className="ml-auto">
+				<Nav.Link href="#home">Logare</Nav.Link>
+				<Nav.Link href="#home">Creare cont</Nav.Link>
+			</Nav>
         </Navbar.Collapse>
-      </Container>
     </Navbar>
   );
+// 	return (
+// 	  <Navbar bg="light" expand="lg">
+// 		<Navbar.Brand href="#">
+// 		  <Image src="https://www.example.com/logo.png" width="30" height="30" />
+// 		</Navbar.Brand>
+// 		<Navbar.Toggle aria-controls="basic-navbar-nav" />
+// 		<Navbar.Collapse id="basic-navbar-nav">
+// 		  <Nav className="ml-auto">
+// 			<NavItem>
+// 			  <Nav.Link href="#">Login</Nav.Link>
+// 			</NavItem>
+// 			<NavItem>
+// 			  <Nav.Link href="#">Create account</Nav.Link>
+// 			</NavItem>
+// 		  </Nav>
+// 		</Navbar.Collapse>
+// 	  </Navbar>
+// 	);
+  
+
 }
 
 export default MyNavbar
