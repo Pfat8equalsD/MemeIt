f997550a47cf8e114f213d7e6d4638c3f0bfc9ee
Andrei.bat <marian.sentiment@gmail.com>
2022-12-13 13:32:22 +0200

    Moving the installed node module where it should be

--- frontend/src/MyNavbar.js
+++ frontend/src/MyNavbar.js
@@ -1,7 +1,24 @@
 import React from "react";
 
-function MyNavbar(){
-	return null;
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function MyNavbar() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default MyNavbar
