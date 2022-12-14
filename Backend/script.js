9a9cdd27c830af6c1cfb0c60a6a6a0c8a7c187ee
Andrei.bat <marian.sentiment@gmail.com>
2022-12-14 21:31:28 +0200

    Register and input validation

--- Backend/script.js
+++ Backend/script.js
@@ -16,6 +16,63 @@ app.get('/', (req, res) =>{
 	res.json({message: "Hello"});
 })
 
+app.post('/register', async (req, res) =>{
+	errs = {};
+	if (!req.body.email) 
+		errs.email = "This field is required"
+	if (!req.body.username){
+		errs.username = "This field is required"
+	}
+	if (!req.body.password)
+		errs.password = "This field is required"
+
+	if(Object.keys(errs).length > 0)
+		return res.status(400).json(errs)
+	if(await User.exists({Username:req.body.username}))
+		errs.username = "Username already exists"
+	if(await User.exists({email:req.body.email}))
+		errs.email = "Email already exists"
+	if(Object.keys(errs).length > 0)
+		return res.status(400).json(errs)
+
+	errm = []
+	erru = []
+	errp = []
+	if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(req.body.email))
+		errm.push("The field must be a valid email")
+	if(!/^.+@stud.acs.upb.ro$/.test(req.body.email))
+		errm.push("The field must end in @stud.acs.upb.ro")
+	let cnt = req.body.username.length
+	if(cnt < 8 || cnt > 32)
+		erru.push("Username must be between 8 and 32 characters")
+	cnt = req.body.password.length
+	if(cnt < 8 || cnt > 32)
+		erru.push("Password must be between 8 and 32 characters")
+	if(errm.length > 0)
+		errs.email = errm
+	if(erru.length > 0)
+		errs.username = erru
+	if(errp.length > 0)
+		errs.password = errp
+
+	if(Object.keys(errs).length > 0)
+		return res.status(400).json(errs)
+	bcrypt.hash(req.body.password, 10, function(err, hash) {
+		const user = new User({
+			Username: req.body.username,
+			email: req.body.email,
+			Password: hash
+		})
+		user.save().then();
+		res.json({
+			username: user.Username,
+			email: user.email,
+			password: hash,
+			id: user._id.toString()
+		})
+	});
+})
+
 app.post('/login', async (req, res) =>{
 	const pass = req.body.password;
 	const name = req.body.username;
@@ -41,10 +98,13 @@ app.get("/memes", (req, res) =>{
 	
 })
 
-app.get("/memes/:id", (req, res) =>{
+app.get("/memes/:id", async (req, res) =>{
 	const id = req.params.id;
-
-	
+	const meme = await Meme.findById(id);
+	res.json({
+		description: meme.description,
+		id: id
+	});
 })
 
 function verifyjwt(req,res,next){
@@ -61,8 +121,22 @@ function verifyjwt(req,res,next){
    }
 }
 
-app.post("/memes", verifyjwt,(req, res) =>{
-	
+app.post("/memes", verifyjwt, async (req, res) =>{
+	let user = await User.findOne({Username: req.username});
+	if(req.body.description.length > 2500)
+		return res.status(400).json({message:"Description must be below 2500 characters"})
+	let meme = new Meme({
+		description: req.body.description,
+		owner: user._id
+	})
+	await meme.save();
+	res.json({
+		description: meme.description,
+		id: meme._id.toString()
+	})
+	user.memes.push(meme._id);
+	await user.save();
+	console.log(user.memes);
 })
 
 
